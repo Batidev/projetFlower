5443372e48a87c140be11f6cb1ef72dc47cefc65
batifoleuse <c.spielmann@it-students.fr>
2020-01-28 10:29:15 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
+// caroussel slider
+
 new Glide('.glide', {
     type: 'carousel',
     startAt: 0,
     perView: 3
 }).mount()
 
+// menu burger
 
+$('#burger').on('click', function() {
+    $('menu').toggleClass('menuVisi')
+})
+
+// rellax
 
 var rellax = new Rellax('.rellax', {
     speed: -2,
@@ -14,3 +22,20 @@ var rellax = new Rellax('.rellax', {
     vertical: true,
     horizontal: false
 });
+
+// fancybox
+
+$('[data-fancybox="images"]').fancybox({
+    afterLoad: function(instance, current) {
+        var pixelRatio = window.devicePixelRatio || 1;
+
+        if (pixelRatio > 1.5) {
+            current.width = current.width / pixelRatio;
+            current.height = current.height / pixelRatio;
+        }
+    }
+});
+
+// AOS
+
+AOS.init();
