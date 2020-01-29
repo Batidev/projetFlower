f4896bcb837564741fb1c65cb2b2aac074dda7fc
batifoleuse <c.spielmann@it-students.fr>
2020-01-29 16:34:04 +0100

    maj

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@
 new Glide('.glide', {
     type: 'carousel',
     startAt: 0,
-    perView: 3
+    perView: 3,
+    breakpoints: {
+        650: {
+            perView: 1
+        }
+    }
 }).mount()
 
 // menu burger
